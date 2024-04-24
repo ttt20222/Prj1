f597f8d96a2329585548a6d23ba79c3ba9676f57
LeeJooeon <leejooeon@LeeJooeonui-MacBookAir.local>
2024-04-24 21:22:16 +0900

    4/24 오후 작업 완료

---
+++ movie.js
@@ -0,0 +1,10 @@
+
+
+const ratingStars = [...document.getElementsByClassName("rating__star")];
+
+function executeRating(stars) {
+    const starClassActive = "rating__star fas fa-star";
+    const starClassInactive = "rating__star far fa-star";
+    const starsLength = stars.length;
+    let i;
+  }
